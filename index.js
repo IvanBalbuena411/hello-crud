d3325f01b341d0ac95260b519a31d9c10050670d
IvanBalbuena411 <iubalbuena@hotmail.com>
2020-10-01 17:11:07 -0300

    primer commit

--- index.js
+++ index.js
@@ -19,28 +19,17 @@ mongoose
     console.log(`DB connected @ ${db}`);
   })
   .catch(err => console.error(`Connection error ${err}`));
-
 // todo el middleware aca abajo y antes del listen
-/** 
-  * los middlewares se ejecutan en orden para todas las peticiones
-  * salvo que no coincida la ruta
-  * (solo para el 4to middleware en este caso)
-  */
-app.use(express.json());                        // 1er middleware
-app.use(cors());                                // 2do middleware
-app.use(morgan('dev'));                         // 3er middleware
-app.use('/api', require('./api/routes/note'));  // 4to middleware
-// si el cliente NO hace una peticion a algun endpoint de la API
-// entonces usamos una ruta que devuelva un status code 404
-// 5to middleware (error 404 not found)
+app.use(express.json());                        
+app.use(cors());
+app.use('/api', require('./api/routes/note'));
+
 app.use((req, res, next) => {
   const err = new Error('Not found');
   err.status = 404;
   next(err);
 });
-// no terminamos la cadena de middlewares ahi
-// la pasamos a un 6to middleware que responda al cliente
-// con el error 404 o 500 si vino de otro lado el problema
+
 app.use((err, req, res, next) => {
   res.status(err.status || 500);
   // para mas detalles usar: console.error(err.stack)
